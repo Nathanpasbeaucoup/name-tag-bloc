be82f1cab15adb264ca8d6260807b36ccef9d731
Nathanpasbeaucoup <92890159+Nathanpasbeaucoup@users.noreply.github.com>
2021-10-26 09:39:11 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.forever(function () {
+    basic.showString("Nathan !")
+    basic.showString("Ma couleur préféré:")
+    basic.showString("Rouge")
+    basic.showString("Mon age:")
+    basic.showString("14")
+})
